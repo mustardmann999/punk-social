fd7bd8c7c56048e7225687e7438651d743915677
mustardmann999 <dan37rib@gmail.com>
2022-10-13 00:54:31 -0400

    Update requirements, secure secret key

--- SocialClone/SocialClone/settings.py
+++ SocialClone/SocialClone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/'
@@ -22,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rv^wm0l(in9!j!)egv8&&s)^x1ylf0419yyc11w@#$xf57!=4_'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
